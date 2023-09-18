8138fd540dea7c104d707a44c7e74acd786f5256
soParkjeong <138647147+soParkjeong@users.noreply.github.com>
2023-09-18 23:51:08 +0900

    [level 0] Title: 편지, Time: 0.01 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String message) {
+        int messageLength=message.length();
+        int paperWidth=2*messageLength;
+        
+        return paperWidth;
+    }
+}
