64b28e7f7b95747f7c5be94cc458662c570690bb
soParkjeong <138647147+soParkjeong@users.noreply.github.com>
2023-09-22 14:32:57 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,8 @@
+
+class Solution {
+    public String solution(String my_string) {
+      StringBuilder result=new StringBuilder(my_string);
+        result.reverse();
+        return result.toString();
+    }
+}
