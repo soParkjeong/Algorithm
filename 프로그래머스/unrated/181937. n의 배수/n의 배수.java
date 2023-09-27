e888f803f43dd5d21fc6544b59b98e732302e9ab
soParkjeong <138647147+soParkjeong@users.noreply.github.com>
2023-09-27 22:03:32 +0900

    [unrated] Title: n의 배수, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181937. n의 배수/n의 배수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int num, int n) {
+        int result=0;
+       if(num%n==0){
+           result=1;
+       }
+        else{
+            result=0;
+        }
+        return result;
+    }
+}
