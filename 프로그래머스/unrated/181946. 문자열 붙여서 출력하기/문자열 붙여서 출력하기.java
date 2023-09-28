390ca3872cf56cd56bc9967557bfece937918eba
soParkjeong <138647147+soParkjeong@users.noreply.github.com>
2023-09-28 14:58:48 +0900

    [unrated] Title: 문자열 붙여서 출력하기, Time: 136.57 ms, Memory: 68.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+       Scanner scanner=new Scanner(System.in);
+       
+        String str1=scanner.next();
+        String str2=scanner.next();
+        
+        System.out.println(str1+str2);
+       
+        scanner.close();
+    }
+}
