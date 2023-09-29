8054d7603e33b3fb058f5a09327743c2a3ed167a
soParkjeong <138647147+soParkjeong@users.noreply.github.com>
2023-09-29 23:21:34 +0900

    [unrated] Title: 정수 찾기, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list, int n) {
+        int result=0;
+        
+        for(int i=0;i<num_list.length;i++){
+            if(num_list[i]==n){
+                result=1;
+                break;
+            }
+            else{
+                result=0;
+            }
+        }
+        return result;
+    }
+}
