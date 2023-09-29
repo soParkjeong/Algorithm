1072a85e4347df59a823b46a050676bea1fb1069
soParkjeong <138647147+soParkjeong@users.noreply.github.com>
2023-09-29 12:22:18 +0900

    [unrated] Title: 공배수, Time: 0.03 ms, Memory: 69.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181936. 공배수/공배수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int number, int n, int m) {
+        int result=0;
+        if(number%n==0&&number%m==0){
+            result=1;
+        }
+        else{
+            result=0;
+        }
+        return result;
+    }
+}
