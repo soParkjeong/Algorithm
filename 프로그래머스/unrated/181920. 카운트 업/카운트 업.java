4fd954ebeeee792e673baf9d2dae2a8fa0428733
soParkjeong <138647147+soParkjeong@users.noreply.github.com>
2023-09-29 23:10:45 +0900

    [unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] result=new int[end_num-start_num+1];
+        int k=0;
+        
+        for(int i=start_num;i<=end_num;i++){
+            result[k]=i;
+            k++;
+        }
+        return result;
+    }
+}
