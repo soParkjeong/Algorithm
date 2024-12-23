886411a706acf19979771b714ffb45fd156d91f3
soParkjeong <138647147+soParkjeong@users.noreply.github.com>
2024-12-24 00:59:45 +0900

    Title: 살 수 있는 물건 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241224/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String pur="";
+
+        if(n>=3000){
+            pur="book";
+        }
+        else if(n>=1000){
+            pur="mask"
+        }
+        else if(n>=500){
+            pur="pen";
+        }
+        else{
+            pur="no";
+        }
+        System.out.print(score);
+    }
+}
