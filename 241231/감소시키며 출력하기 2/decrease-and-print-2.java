e09b734a195d3315ca0a3f6bf2bd2f7d3bcea784
soParkjeong <138647147+soParkjeong@users.noreply.github.com>
2024-12-31 17:59:59 +0900

    Title: 감소시키며 출력하기 2, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241231/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int i=26;
+        while(i>=10){
+            System.out.print(i+" ");
+            i--;
+        }
+    }
+}
