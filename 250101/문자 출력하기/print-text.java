2d278809f249a296499099fd4925a8db046c15bf
soParkjeong <138647147+soParkjeong@users.noreply.github.com>
2025-01-01 12:28:49 +0900

    Title: 문자 출력하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250101/문자 출력하기/print-text.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String n=sc.next();
+        for(int i=0;i<=7;i++){
+            System.out.print(n);
+        }
+    }
+}
