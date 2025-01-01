2e9f1a9f2ed0217111d7e2f438f1f1320f370863
soParkjeong <138647147+soParkjeong@users.noreply.github.com>
2025-01-01 13:51:25 +0900

    Title: a/b 출력, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 250101/a/b 출력/a-divide-b.java
+++ 250101/a/b 출력/a-divide-b.java
@@ -8,11 +8,9 @@ public class Main {
         
         System.out.print(a/b+".");
 
-        a=a%b;
         for(int i=0;i<20;i++){
-            a=a*10;
+            a=(a%b)*10;
             System.out.print(a/b);
-            a=a%b;
         }
         
     }
