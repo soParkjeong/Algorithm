f6cbea4f81065a288c2a735f81d20e10857606d1
soParkjeong <138647147+soParkjeong@users.noreply.github.com>
2025-01-01 13:25:39 +0900

    Title: 문자열 출력하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 250101/문자열 출력하기/print-string.java
+++ 250101/문자열 출력하기/print-string.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        for(int i=0;i<=5;i++){
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
             System.out.println("LeebrosCode");
         }
     }
