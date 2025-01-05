b61a1fcab6e6bba8af137ce99492e584628e2db4
soParkjeong <138647147+soParkjeong@users.noreply.github.com>
2025-01-05 22:53:31 +0900

    Title: 별표 출력하기, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250105/별표 출력하기/print-star.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int a=1;a<n;a++){
+            for(int b=0;b<n-a;b++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
