e570be532c95efc92b9f342a638b33d8988ad4b8
soParkjeong <138647147+soParkjeong@users.noreply.github.com>
2025-03-01 21:43:41 +0900

    Title: 정수 명령 처리, Time: 377ms, Memory: 20MB, Status: Passed - CodeTree

--- 250301/정수 명령 처리/process-numeric-commands.java
+++ 250301/정수 명령 처리/process-numeric-commands.java
@@ -1,14 +1,18 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        sc.nextLine();
         Stack<Integer> stack=new Stack<>();
 
         for(int i=0;i<n;i++){
-            String[] order=new String[2];
+            String str=sc.nextLine().trim();
+            String[] order=str.split(" ");
+            
             if(order[0].equals("push")){
-                stack.push(order[1]);
+                stack.push(Integer.parseInt(order[1]));
             }else if(order[0].equals("size")){
                 System.out.println(stack.size());
             }else if(order[0].equals("empty")){
@@ -18,9 +22,13 @@ public class Main {
                     System.out.println(0);
                 }
             }else if(order[0].equals("pop")){
-                System.out.println(stack.pop());
-            }else{
-                System.out.println(stack.peek());
+                if(!stack.isEmpty()){
+                    System.out.println(stack.pop());
+                }
+            }else if(order[0].equals("top")){
+            	if(!stack.isEmpty()) {
+            		System.out.println(stack.peek());
+            	}
             }
         }
     }
