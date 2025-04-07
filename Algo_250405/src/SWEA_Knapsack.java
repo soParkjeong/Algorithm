21a907e9a4e152b40eea826498cef01a53cfee07
soParkjeong <sojung0734@naver.com>
2025-04-07 11:09:18 +0900

    "250405"

---
+++ Algo_250405/src/SWEA_Knapsack.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SWEA_Knapsack {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int T=Integer.parseInt(br.readLine().trim());
+	
+		for(int tc=1;tc<=T;tc++) {
+			StringTokenizer st=new StringTokenizer(br.readLine());
+			int N=Integer.parseInt(st.nextToken());
+			int K=Integer.parseInt(st.nextToken());
+			
+			int[] W=new int[N+1];
+			int[] V=new int[N+1];
+			
+			for(int i=1;i<=N;i++) {
+				st=new StringTokenizer(br.readLine());
+				W[i]=Integer.parseInt(st.nextToken());
+			}
+		}
+	}
+}
