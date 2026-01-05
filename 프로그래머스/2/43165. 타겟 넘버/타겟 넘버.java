df69915c36cbeac7f8134d5f0cc0f04a488f0eb4
soParkjeong <138647147+soParkjeong@users.noreply.github.com>
2026-01-05 23:56:19 +0900

    [level 2] Title: 타겟 넘버, Time: 6.92 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,18 @@
+class Solution {
+    int answer = 0; 
+
+    public int solution(int[] numbers, int target) {
+        dfs(0, 0, numbers, target); 
+        return answer;
+    }
+
+    void dfs(int idx, int sum, int[] numbers, int target) {
+        if (idx == numbers.length) {
+            if (sum == target) answer++;
+            return; 
+        }
+
+        dfs(idx + 1, sum + numbers[idx], numbers, target);
+        dfs(idx + 1, sum - numbers[idx], numbers, target);
+    }
+}
