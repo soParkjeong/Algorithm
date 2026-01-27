c3bfb8ba37d222456111d14f56fa7eaf4262c11a
soParkjeong <138647147+soParkjeong@users.noreply.github.com>
2026-01-27 21:24:44 +0900

    [level 3] Title: 섬 연결하기, Time: 1.42 ms, Memory: 90.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Solution {
+    
+    static class Edge {
+        int to,cost;
+
+        public Edge(int to,int cost){
+            this.to=to;
+            this.cost=cost;
+        }
+    }
+    
+    public int solution(int n, int[][] costs) {
+        //모든 섬이 서로 통행 가능하도록 한다?
+        //모든 정점 연결, 최소 비용-> 프림 ㄱ
+        List<Edge>[]graph=new ArrayList[n];
+        for(int i=0;i<n;i++) graph[i]=new ArrayList<>();
+        
+        for(int []e: costs){
+            int a=e[0];
+            int b=e[1];
+            int c=e[2];
+            graph[a].add(new Edge(b,c));
+            graph[b].add(new Edge(a,c));
+        }
+        
+        boolean[] visited=new boolean[n];
+        PriorityQueue<Edge>pq=new PriorityQueue<>((a,b)->Integer.compare(a.cost,b.cost));
+        
+        pq.offer(new Edge(0,0));
+        
+        int total=0;
+        int picked=0;
+
+        while(!pq.isEmpty()){
+            Edge cur=pq.poll();
+            int now=cur.to;
+
+            if(visited[now]) continue;
+            visited[now]=true;
+
+            total+=cur.cost;
+            picked++;
+
+            for(Edge next:graph[now]){
+                if(!visited[next.to]){
+                    pq.offer(next);
+                }
+            }
+        }
+        
+        return total;
+    }
+}
