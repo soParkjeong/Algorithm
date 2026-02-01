5fbfffc522b7658a92e7b9ad90524c5d0ff35c9a
soParkjeong <138647147+soParkjeong@users.noreply.github.com>
2026-02-01 17:07:44 +0900

    Time: 1 ms (99.2%), Space: 45.5 MB (58.4%) - LeetHub

--- 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.java
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.java
@@ -3,15 +3,25 @@ import java.util.*;
 class Solution {
     public int minimumCost(int[] nums) {
         //원래 nums배열 순서 유지-정렬 안됨
-        //그러면 첫 비용은 nums[0]
-        //나머지는 걍 정렬해서 최솟값 2개 뽑기
+        //그러면 첫 비용은 무조건 nums[0]
+        //나머지는 최솟값 2개 뽑기
+        //정렬 안쓰는 방식
         int ans=nums[0];
         
-        Arrays.sort(nums, 1, nums.length);
-        
-        for(int i=1;i<3;i++){
-            ans+=nums[i];
+        int min1=Integer.MAX_VALUE;
+        int min2=Integer.MAX_VALUE;
+
+        for (int i= 1; i< nums.length; i++) {
+            int x= nums[i];
+
+            if (x<min1) {      
+                min2=min1;       
+                min1=x;
+            } else if (x<min2) { 
+                min2=x;
+            }
         }
+        ans+=(min1+min2);
         return ans;
     }
 }
